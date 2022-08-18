efd7294929504bb9967ac0ba3c2aab14b60320dd
Guilherme Gama <guilhermegamabs@gmail.com>
2022-08-17 21:21:49 -0300

    Atualizado

---
+++ js-basico/aula011/js/ex.js
@@ -0,0 +1,8 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+let ajuda = null;
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
---
+++ js-basico/aula011/js/index.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt('Digite um número'));
+let num2 = Number(prompt('Digite outro número:'));
+const soma = num1 + num2;
+alert(`O resultado foi ${soma}`);
+
+console.log(typeof num1);
