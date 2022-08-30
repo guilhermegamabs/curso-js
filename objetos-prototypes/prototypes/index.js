418b39b35ee8cd5dacb6bcc60a28b8d9c135c331
Guilherme Gama <guilhermegamabs@gmail.com>
2022-08-29 21:45:31 -0300

    a

--- objetos-prototypes/prototypes/index.js
+++ objetos-prototypes/prototypes/index.js
@@ -1,2 +1,18 @@
 // Construtora -> molde (classe)
-function
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => 'ORIGINAL: ' + this.nome + ' ' + this.sobrenome;
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return this.nome + ' ' + this.sobrenome;
+}
+
+//instância
+const pessoa1 = new Pessoa('Luiz', 'O.'); //<-Pessoa = Função Construtora
+const pessoa2 = new Pessoa('Maria', 'A.'); // <-Pessoa = Função Construtora
+
+const data = new Date();
+console.dir(pessoa1);
+console.dir(data);
