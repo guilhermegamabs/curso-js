c4cd00abe06bc8e2e9ee5a5974caa2e672a378c8
Guilherme Gama <guilhermegamabs@gmail.com>
2022-09-04 16:19:25 -0300

    Atualizado

--- tooling-es6-modules/web-pack/webpack.config.js
+++ tooling-es6-modules/web-pack/webpack.config.js
@@ -7,7 +7,18 @@ module.exports = {
         path: path.resolve(__dirname, 'public', 'assets', 'js'), // dirname é o repositório atual
         filename: 'bundle.js'
     },
-    module: {},
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
     devtool: 'source-map'
 };
 
